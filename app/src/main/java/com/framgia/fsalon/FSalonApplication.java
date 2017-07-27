9c283647c7bf5ba1f5b4cf31ecf57ea5668d9e9e
ToanDoan <toandoan.coder@gmail.c>
2017-07-27 13:42:40 +0700

    tesst ci

--- app/src/main/java/com/framgia/fsalon/FSalonApplication.java
+++ app/src/main/java/com/framgia/fsalon/FSalonApplication.java
@@ -1,13 +1,15 @@
 package com.framgia.fsalon;
 
 import android.app.Application;
-
 import com.framgia.fsalon.data.source.api.FSalonServiceClient;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MyPC on 20/07/2017.
  */
 public class FSalonApplication extends Application {
+
     private static FSalonApplication sInstant;
 
     @Override
@@ -15,6 +17,24 @@ public class FSalonApplication extends Application {
         super.onCreate();
         FSalonServiceClient.initialize(this);
         sInstant = this;
+        List<String> a = new ArrayList<>();
+        a.add("ABCCCCCCCCCCCCCCC");
+        a.add("ABCCCCCCCCCCCCCCC");
+        a.add("ABCCCCCCCCCCCCCCC");
+        a.add("ABCCCCCCCCCCCCCCC");
+        a.add("ABCCCCCCCCCCCCCCC");
+        a.add("ABCCCCCCCCCCCCCCC");
+        a.add(
+            "ABCCCCCCCCCCCÁDSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSssssssssssssssssssssssssssssssssssssSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSCCCC");
+        a.add(
+            "ABCCCCCCCCCCCÁDSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSCCCC");
+        a.add(
+            "ABCCCCCCCCCCCÁDSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSssssssssssssssssssssssssssssssssssssssssssSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSCCCC");
+        a.add(
+            "ABCCCCCCCCCCCÁDSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSssssssssssssssssssssssssssssssssSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSCCCC");
+        a.add(
+            "ABCCCCCCCCCCCÁDSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSssssssssssssssssssssssssssSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSCCCC");
+        a.add("ABCCCCCCCCCCCCCCC");
     }
 
     public static FSalonApplication getInstant() {
