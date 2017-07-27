b1e07b3ca9ccc2599c0ec2d1d94b2e5d6a114d8d
ToanDoan <toandoan.coder@gmail.c>
2017-07-27 08:58:12 +0700

    Test Ci

--- app/src/main/java/com/framgia/fsalon/FSalonApplication.java
+++ app/src/main/java/com/framgia/fsalon/FSalonApplication.java
@@ -1,13 +1,15 @@
 package com.framgia.fsalon;
 
 import android.app.Application;
-
 import com.framgia.fsalon.data.source.api.FSalonServiceClient;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MyPC on 20/07/2017.
  */
 public class FSalonApplication extends Application {
+
     private static FSalonApplication sInstant;
 
     @Override
@@ -15,6 +17,25 @@ public class FSalonApplication extends Application {
         super.onCreate();
         FSalonServiceClient.initialize(this);
         sInstant = this;
+        List<String> a = new ArrayList<>();
+        a.add("ABC");
+        a.add("ABC");
+        a.add("ABC");
+        a.add("ABC");
+        a.add("ABC");
+        a.add("ABC");
+        a.add(
+            "ádasdljsakdljaslkdjaskldjaskldjaslkdjaskldjaskldjaskldjaslkdjsalkdjaslkdjaslkdjasdkljsalkdjsakldjaslkdjaskldjaskldjaskldjasdklasjdklsajdklsajdklsajdklsajdsa");
+
+        a.add(
+            "ádasdljsakdljaslkdjaskldjaskldjaslkdjaskldjaskldjaskldjaslkdjsalkdjaslkdjaslkdjasdkljsalkdjsakldjaslkdjaskldjaskldjaskldjasdklasjdklsajdklsajdklsajdklsajdsa");a.add(
+            "ádasdljsakdljaslkdjaskldjaskldjaslkdjaskldjaskldjaskldjaslkdjsalkdjaslkdjaslkdjasdkljsalkdjsakldjaslkdjaskldjaskldjaskldjasdklasjdklsajdklsajdklsajdklsajdsa");a.add(
+            "ádasdljsakdljaslkdjaskldjaskldjaslkdjaskldjaskldjaskldjaslkdjsalkdjaslkdjaslkdjasdkljsalkdjsakldjaslkdjaskldjaskldjaskldjasdklasjdklsajdklsajdklsajdklsajdsa");a.add(
+            "ádasdljsakdljaslkdjaskldjaskldjaslkdjaskldjaskldjaskldjaslkdjsalkdjaslkdjaslkdjasdkljsalkdjsakldjaslkdjaskldjaskldjaskldjasdklasjdklsajdklsajdklsajdklsajdsa");a.add(
+            "ádasdljsakdljaslkdjaskldjaskldjaslkdjaskldjaskldjaskldjaslkdjsalkdjaslkdjaslkdjasdkljsalkdjsakldjaslkdjaskldjaskldjaskldjasdklasjdklsajdklsajdklsajdklsajdsa");
+
+        a.add(
+            "ádasdljsakdljaslkdjaskldjaskldjaslkdjaskldjaskldjaskldjaslkdjsalkdjaslkdjaslkdjasdkljsalkdjsakldjaslkdjaskldjaskldjaskldjasdklasjdklsajdklsajdklsajdklsajdsa");
     }
 
     public static FSalonApplication getInstant() {
