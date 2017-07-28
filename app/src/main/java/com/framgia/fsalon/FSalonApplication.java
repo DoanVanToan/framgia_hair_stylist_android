f1aa2df50599c8f54689fed5cf9d73f0aba05d96
ToanDoan <toandoan.coder@gmail.c>
2017-07-28 09:19:24 +0700

    config ci

--- app/src/main/java/com/framgia/fsalon/FSalonApplication.java
+++ app/src/main/java/com/framgia/fsalon/FSalonApplication.java
@@ -5,6 +5,8 @@ import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.ndk.CrashlyticsNdk;
 import com.framgia.fsalon.data.source.api.FSalonServiceClient;
 import io.fabric.sdk.android.Fabric;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MyPC on 20/07/2017.
@@ -12,6 +14,7 @@ import io.fabric.sdk.android.Fabric;
 public class FSalonApplication extends Application {
 
     private static FSalonApplication sInstant;
+    private final static int a = 10;
 
     @Override
     public void onCreate() {
@@ -19,6 +22,12 @@ public class FSalonApplication extends Application {
         Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         FSalonServiceClient.initialize(this);
         sInstant = this;
+        List<String> a = new ArrayList<>();
+        a.add("ABC");
+        a.add("ABC");
+        a.add("ABCccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc");
+        a.add("ABCccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc");
+        a.add("ABCccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc");
     }
 
     public static FSalonApplication getInstant() {
